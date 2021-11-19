3cb4fb562e78d69722f2296f41447e6710000070
medi.demirdelen1 <medi.demirdelen@gmail.com>
2021-11-19 14:23:09 +0100

    lecture du json

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
-var filesystem = require("fs");
-var results = [];
+var fs = require("fs");
+var fileList = [];
+var filterList = [];
 const dir = "./filters";
+const config = './config-filters.json';
+let configData;
 
-filesystem.readdirSync(dir).forEach(function(file) {
+fs.readdirSync(dir).forEach(function(file) {
 
-    file = dir+'/'+file;
-    var stat = filesystem.statSync(file);
+    file = `${dir}/${file}`;
+    fileList.push(file);
+    if (require(file) instanceof Function) filterList.push(require(file));
+    else console.error(`${file} n'a pas de fonction`);
 
-    if (stat && stat.isDirectory()) {
-        results = results.concat(_getAllFilesFromFolder(file))
-    } else {
-        results.push(require(file));
-    } 
+});
 
+fs.readFile(config, (err, data) => {
+    if (err) console.error(`erreur lecture config json`);
+    configData = JSON.parse(data);
+    console.log(configData);
 });
