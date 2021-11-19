e7d77cf63c80d2b699ffaa426178f34c0d0df869
Guillaume Gouy <guillaumegouy@sfr.fr>
2021-11-19 13:52:26 +0100

    lecture dossier filtres

---
+++ app.js
@@ -0,0 +1,14 @@
+var filesystem = require("fs");
+var results = [];
+const dir = "./filters";
+
+filesystem.readdirSync(dir).forEach(function(file) {
+
+    file = dir+'/'+file;
+    var stat = filesystem.statSync(file);
+
+    if (stat && stat.isDirectory()) {
+        results = results.concat(_getAllFilesFromFolder(file))
+    } else results.push(file);
+
+});
