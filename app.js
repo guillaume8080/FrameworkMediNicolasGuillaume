c0d53c1adb8c31b2eea40baedf77f998e04b1ff1
medi.demirdelen1 <medi.demirdelen@gmail.com>
2021-11-19 14:59:02 +0100

    console log éxecution filter dans l'ordre

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ var fileList = [];
 var filterList = [];
 const dir = "./filters";
 const config = './config-filters.json';
-let configData;
+let configData = Object;
 
-fs.readdirSync(dir).forEach(function(file) {
+fs.readdirSync(dir).forEach(function (file) {
 
     file = `${dir}/${file}`;
     fileList.push(file);
@@ -17,5 +17,35 @@ fs.readdirSync(dir).forEach(function(file) {
 fs.readFile(config, (err, data) => {
     if (err) console.error(`erreur lecture config json`);
     configData = JSON.parse(data);
-    console.log(configData);
+    //console.log(configData);
+    //console.log(typeof(configData));
+    //console.log(configData.steps);
+
+    //vérification du config
+    steps = configData.steps
+    if (steps == {}) console.error("le fichier config n'a pas de steps");
+    else { //vérification des steps et nom de filters
+        for (let step = 1; step <= Object.keys(steps).length; step++) {
+            thisStep = steps[step.toString()];
+            if (thisStep.filter == '') console.error(`le filtre numéro ${step} n'a pas de nom filter`);
+        }
+
+        let filterOrder = [];
+        let nextStep;
+        filterOrder.push(steps['1'].filter);
+
+        if (steps['1'].next != "") {
+            nextStep = steps['1'].next;
+            while (nextStep > 0) {
+                filterOrder.push(steps[nextStep].filter);
+                nextStep = steps[nextStep].next || 0;
+            }
+        } 
+
+        filterOrder.forEach((f) => {
+            console.log(`Exécution du filter ${f}`);
+        });
+
+    }
+
 });
